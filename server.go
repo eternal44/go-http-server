3690de4bd8d5eb00c5d387ace6b0e8dddafb30b7
James Youn <jamesyoun710@gmail.com>
2020-05-01 19:53:27 -0700

    WIP: savepoint

--- server.go
+++ server.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+  "http/server/request/handlers"
+)
 
 func main() {
   fmt.Println("hello")
+	// http.HandleFunc("/view/", makeHandler(viewHandler))
+	// http.HandleFunc("/edit/", makeHandler(editHandler))
+	// http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	// log.Fatal(http.ListenAndServe(":8080", nil))
 }
