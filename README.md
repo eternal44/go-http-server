6c6cc852f1dd31d754d7e1c505970210c939da8e
James Youn <jamesyoun710@gmail.com>
2020-05-03 09:26:55 -0700

    Add go fmt fixes and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Example Server
+This is an HTTP server with some basic configs (middleware chaining, request route handling).
--- server.go
+++ server.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"net/http"
+	"github.com/gorilla/mux"
 	"log"
-  "github.com/gorilla/mux"
-  "sourcegraph/server/request"
-  "sourcegraph/server/middleware"
+	"net/http"
+	"sourcegraph/server/middleware"
+	"sourcegraph/server/request"
 )
 
 func main() {
-  r := mux.NewRouter()
-  r.HandleFunc("/view/{topic}", middleware.Chain(
-    handlers.View,
-    middleware.Log,
-  ))
-  r.HandleFunc("/edit/{topic}", middleware.Chain(
-    handlers.Edit,
-    middleware.Log,
-  ))
-  r.HandleFunc("/save/{topic}", middleware.Chain(
-    handlers.Save,
-    middleware.Log,
-  ))
-  http.Handle("/", r)
+	r := mux.NewRouter()
+	r.HandleFunc("/view/{topic}", middleware.Chain(
+		handlers.View,
+		middleware.Log,
+	))
+	r.HandleFunc("/edit/{topic}", middleware.Chain(
+		handlers.Edit,
+		middleware.Log,
+	))
+	r.HandleFunc("/save/{topic}", middleware.Chain(
+		handlers.Save,
+		middleware.Log,
+	))
+	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
